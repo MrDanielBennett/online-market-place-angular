721321746b9ff05dca1c6912855c24ab30cba7a5
Alex Garcia and Daniel Bennett <daniel@>
2019-01-29 15:43:31 -0800

    add services to routing

--- src/app/album-deatail/album-deatail.component.ts
+++ src/app/album-deatail/album-deatail.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Location } from '@angular/common';
+import { Album } from '../album.model';
+import { AlbumService } from '../album.service';
 
 @Component({
   selector: 'app-album-deatail',
   templateUrl: './album-deatail.component.html',
-  styleUrls: ['./album-deatail.component.css']
+  styleUrls: ['./album-deatail.component.css'],
+  providers: [AlbumService]
 })
-export class AlbumDeatailComponent implements OnInit {
 
-  constructor() { }
+
+export class AlbumDeatailComponent implements OnInit {
+  albumId: number;
+  albumToDisplay: Album;
+  constructor(
+    private route: ActivatedRoute,
+    private location: Location,
+    private ablumService: AlbumService
+  ) {}
+
 
   ngOnInit() {
+    this.route.params.forEach((urlParameters) => {
+      this.albumId = parseInt(urlParameters['id']);
+    });
+    this.albumToDisplay = this.ablumService.getAlbumById(this.albumId);
   }
 
 }
