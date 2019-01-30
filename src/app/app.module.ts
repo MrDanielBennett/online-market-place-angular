34b062d4b7781775f856344d334c993a308186d7
christopher cahill and daniel bennett <daniel@>
2019-01-30 10:05:34 -0800

    add new admin component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { AlbumDeatailComponent } from './album-deatail/album-deatail.component';
 import { masterFirebaseConfig } from './api-keys';
 import { AngularFireModule } from 'angularfire2';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AdminComponent } from './admin/admin.component';
 
 export const firebaseConfig = {
   apiKey: masterFirebaseConfig.apiKey,
@@ -25,7 +26,8 @@ export const firebaseConfig = {
     WelcomeComponent,
     AboutComponent,
     MarketplaceComponent,
-    AlbumDeatailComponent
+    AlbumDeatailComponent,
+    AdminComponent
   ],
   imports: [
     BrowserModule,
